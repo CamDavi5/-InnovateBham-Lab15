741224a7798bbb6fa91e63bf85f8dc898e85c6f1
CamDavi5 <camrdavis.biz@gmail.com>
2022-03-22 13:44:32 -0500

    styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="nav nav-pills navbar-success justify-content-center p-2 shadow">
-            <NavLink to="/" className={({ isActive }) => `nav-link ${isActive && 'active'}`}>
+        <nav className="nav nav-pills nav-pills-warning bg-secondary justify-content-center p-2 shadow">
+            <NavLink to="/" className={({ isActive }) => `nav-link text-white ${isActive && 'active'}`}>
                 Home
             </NavLink>
-            <NavLink to="/films" className={({ isActive }) => `nav-link ${isActive && 'active'}`}>
+            <NavLink to="/films" className={({ isActive }) => `nav-link text-white ${isActive && 'active'}`}>
                 Films
             </NavLink>
-            <NavLink to="/people" className={({ isActive }) => `nav-link ${isActive && 'active'}`}>
+            <NavLink to="/people" className={({ isActive }) => `nav-link text-white ${isActive && 'active'}`}>
                 People
             </NavLink>
         </nav>
